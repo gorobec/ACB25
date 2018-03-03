f99849cc9db12615288fc92651c3fb1a8363abf9
gorobec <yevhenijvorobiei@gmail.com>
2018-03-03 11:56:28 +0200

    WEEK2 pizza day

--- src/week2/Array.java
+++ src/week2/Array.java
@@ -1,8 +1,41 @@
 package week2;
 
+import java.util.Arrays;
+
 /**
  * @author Yevhen Vorobiei
  * @since JDK 1.8
  */
 public class Array {
+    public static void main(String[] args) {
+        char[] array = new char[10];
+
+
+        array[0] = 'A';
+        array[1] = 'b';
+        array[9] = 'Z';
+//        array[10] = '8';
+
+        int[] arr = {1, 5, 89, -8, 7};
+
+
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+
+//        String asString = Arrays.toString(arr);
+        System.out.println(Arrays.toString(arr));
+
+        for(int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+
+        }
+
+    int[] arr2 = null;
+
+        if(arr2 != null) {
+            arr2[0] = 5;
+        }
+    }
 }
